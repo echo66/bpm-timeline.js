1e6ceba7401925d66c20be49bcaf74f92ea1a287
Bruno Dias <bruno.filipe.silva.dias@gmail.com>
2015-05-04 17:22:04 +0100

    Fixed a 'mistake' in the matlab code. In the next commit, I will update the javascript code.

--- matlab code/fn_linear_integral_inverse.m
+++ matlab code/fn_linear_integral_inverse.m
@@ -2,12 +2,12 @@ function res = fn_linear_integral_inverse (x0, x1, y0, y1, constant, y)
 	dx    = x1 - x0;
 	dy    = y1 - y0;
     A    = 0.5 * dy / dx;
-    B    = y0 - 2 * A * x0;
-    C    = A * (x0.^2) - y0 * x0 + constant;
+    B    = y0;
+    C    = constant;
     
-    square_root = sqrt(B*B - 4*A*(C-y));
+    square_root = sqrt(B*B - 4*A.*(C-y));
     sol1 = (-B + square_root) / (2*A);
     sol2 = (-B - square_root) / (2*A);
     
-    res = [sol1' sol2'];
+    res = [sol1+x0 sol2+x0];
 end
--- matlab code/test.m
+++ matlab code/test.m
@@ -3,44 +3,39 @@ close all; clear; clc;
 figure
 hold
 
+X = [ 0   50;
+      50  110;
+      110 120;
+      120 125;
+      125 130 ];
+  
+Y = [ 60  120;
+      120 140;
+      140 50;
+      50  10;
+      10  1000 ];
 
-x0 = 0;
-x1 = 100;
-y0 = 1;
-y1 = 20;
-
-X1 = [x0:0.1:x1];
-l1 = fn_exponential(x0, x1, y0, y1, X1);
-i1 = fn_exponential_integral(x0, x1, y0, y1, 0, X1);
-ii1 = fn_exponential_integral_inverse(x0, x1, y0, y1, 0, i1);
-plot(X1, l1, 'red')
-plot(X1, i1, 'blue')
-plot(X1, ii1, 'green')
-
-
-
-% x0 = x1 
-% x1 = 2*x1;
-% y0 = y1;
-% y1 = 10*y1;
-% 
-% X1 = [x0:0.1:x1];
-% l1 = fn_linear(x0, x1, y0, y1, X1);
-% i1 = fn_linear_integral(x0, x1, y0, y1, i1(end), X1);
-% plot(X1, l1, 'red')
-% plot(X1, i1, 'red')
-% 
-% 
-% 
-% x0 = x1
-% x1 = 2*x1;
-% y0 = y1;
-% y1 = 0.01*y1;
-% 
-% X1 = [x0:0.1:x1];
-% l1 = fn_linear(x0, x1, y0, y1, X1);
-% i1 = fn_linear_integral(x0, x1, y0, y1, i1(end), X1);
-% plot(X1, l1, 'blue')
-% plot(X1, i1, 'blue')
-% 
-% hold off
+ last = 0;
+ last_ = 0;
+ 
+ X_  = [];
+ I_  = [];
+ II_ = [];
+  
+for line=1:5
+    x0 = X(line,1);
+    x1 = X(line,2);
+    y0 = Y(line,1);
+    y1 = Y(line,2);
+    X1 = x0:0.1:x1;
+    constant = last;
+    I  = fn_linear_integral(x0, x1, 60/y0, 60/y1, constant, X1)';
+    II = fn_linear_integral_inverse(x0, x1, 60/y0, 60/y1, I(1), I);
+    II = II(:,1)';
+%     plot(X1, I, 'Color', [rand, rand, rand]);
+    plot(X1, II, 'Color', [rand, rand, rand]);
+    last = I(end);
+    X_  = [X_  X1];
+    I_  = [I_  I'];
+    II_ = [II_ II];
+end
